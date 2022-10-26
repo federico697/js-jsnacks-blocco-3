44d73d3a0f46542c85f6b2daeeab7a7a82ba5664
federico697 <federico697697@gmail.com>
2022-10-26 15:43:13 +0200

    fine esercizio senza snack 03

--- snack-01/main.js
+++ snack-01/main.js
@@ -1,21 +1,12 @@
-//variable sum
-let somma = 0;
 
-//create an empty array
+let somma = 0;
 let array = [];
 
-
-//check untill the sum lower than 50
 while(somma < 50){
-    //ask the number
     let number = parseInt(prompt("Inserisci un numero"));
-    //add the number in a sum
     somma += number;
-    //add the value in the array
     array.push(number);
 }
 
-//print the array
 console.log(array);
-//print the sum
 console.log("la somma è: ", somma);
--- snack-04/main.js
+++ snack-04/main.js
@@ -3,23 +3,15 @@ In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, c
 Interrompi il ciclo appena il nome è stato trovato.
 */
 
-
-//list guest
 const guest = ["federico" , "matteo" , "alessia" , "mattia"]
-
-
-//ask the name of the guest
 let nameGuest = prompt("Inserisci il tuo nome: ");
 
-//variable
 let i=0;
 
 while (true){
-    //check if the name is into the guest's list
     if (nameGuest == guest[i]){
         console.log("prego può entrare");
         break;
-    //if you finished the list you're not allowed to enter
     } else if (i = (guest.length - 1)){
         console.log("il suo nome non è sulla lista");
         break;
